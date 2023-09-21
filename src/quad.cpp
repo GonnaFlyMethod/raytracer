de17445bdd8112ac00db2b7865b1c534de20b957
GonnaFlyMethod <alexandergusakov@yahoo.com>
2023-09-21 12:29:03 +0300

    fixing misspels related to quad drawing

--- src/quad.cpp
+++ src/quad.cpp
@@ -8,7 +8,7 @@ Quad::Quad(
 
     CommonMath::Vec3 n = cross(u, v);
     this->normal = unit_vector(n);
-    this->constant_in_plane_equation = dot(q,  normal);
+    this->constant_in_plane_equation = dot(this->normal,  this->q_point);
 
     this->constant_vec_for_finding_alpha_beta = n / CommonMath::dot(n, n);
 
@@ -36,10 +36,10 @@ bool Quad::hit(const CommonMath::Ray &r, Interval ray_t, hit_record &rec) const 
     }
 
     CommonMath::Point3 intersection_point = r.at(appropriate_direction_scaler);
-    CommonMath::Vec3 quad_hit_point = intersection_point - this->q_point;
+    CommonMath::Vec3 quad_hit_vec = intersection_point - this->q_point;
 
-    double alpha = CommonMath::dot(this->constant_vec_for_finding_alpha_beta, CommonMath::cross(quad_hit_point, this->u));
-    double beta = CommonMath::dot(this->constant_vec_for_finding_alpha_beta, CommonMath::cross(this->u, quad_hit_point));
+    double alpha = CommonMath::dot(this->constant_vec_for_finding_alpha_beta, CommonMath::cross(quad_hit_vec, this->v));
+    double beta = CommonMath::dot(this->constant_vec_for_finding_alpha_beta, CommonMath::cross(this->u, quad_hit_vec));
 
     if(alpha < 0 || alpha > 1 || beta < 0 || beta > 1){
         return false;
